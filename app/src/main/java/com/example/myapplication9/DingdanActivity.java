eace450fe948fe0a2ca180893de64e9f22015ded
bingnan <bingnan@qq.com>
2017-05-12 23:28:37 +0800

    hh

--- app/src/main/java/com/example/myapplication9/DingdanActivity.java
+++ app/src/main/java/com/example/myapplication9/DingdanActivity.java
@@ -9,5 +9,6 @@ public class DingdanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dingdan);
+        setContentView(R.layout.activity_dingdan);
     }
 }
